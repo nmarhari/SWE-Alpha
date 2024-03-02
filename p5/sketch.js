d1624b6e3402e834005ca05d60e9e69a89ef0424
HadjAhmed <hadj_ahmed@aucegypt.edu>
2024-03-02 12:48:22 -0500

    incorporating scene manager to the most updated version

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,6 +1,6 @@
 //globals
-let start; //canvas for start screen
-let startVisible = true; // renders start screen once
+// let start; //canvas for start screen
+// let startVisible = true; // renders start screen once
 
 let death;
 let deathVisible = false;
@@ -32,16 +32,12 @@ function setup() {
 	canvas = createCanvas(windowWidth, windowHeight, WEBGL);
   	start = createGraphics(windowWidth, windowHeight, WEBGL);
   	death = createGraphics(windowWidth, windowHeight, WEBGL);
-
-  	strokeWeight(0.04);
- 	textFont(f);
- 	textSize(12);
-  	ball = new FireBall(10, -20, 10, 2);
-  	player = new Player();
-  	maze = new Maze(22,12);
- 	maze.setPlayerAtStart(player);
- 	frameRate(60);
-  	strokeWeight(2);
+	manager = new SceneManager();
+	// Preload scenes. Preloading is normally optional
+	// ... but needed if showNextScene() is used.
+	manager.addScene ( startscene );
+	manager.addScene ( gamescene );
+	manager.showNextScene()
 }
 
 // viewport resize when window size changes
@@ -56,35 +52,111 @@ window.addEventListener('resize', async function(event){
   startButton.position(windowWidth/2/2, 250);
 });
 
-function keyPressed() {
-	if (key == 'h') help = !help;
-	if(key=='+'){
-		player.pov.fovy -= 0.1;
-		player.updatePOV();
-	}
-	if(key=='-'){
-		player.pov.fovy += 0.1;
-	player.updatePOV();
-	}
+function draw()
+{
+    manager.draw();
 }
 
-function draw() {
-	frameRate(60);
-  	background(0, 0, 51);
+function mousePressed()
+{
+    manager.handleEvent("mousePressed");
+}
 
-  	if(frameCount % 30 === 0){
-      	maze.checkLavaCollision(player);
-		ball.checkCollision(player);
-  	}
+function keyPressed()
+{
+    manager.handleEvent("keyPressed");
+}
+
+function startscene()
+{
+  this.setup = function()
+  {
+    background(0); 
+    fill('orange');
+    textFont(f);
+    textSize(60);
+    textAlign(CENTER);
+    text('Floor is Lava', 0, 0);
+    text('press Enter to start', 0, 60)
+
+  }
+  this.keyPressed = function()
+  {
+    if(keyCode== ENTER){
+      manager.showNextScene(); 
+    }
+  }
+}
+function gamescene()
+{
+	this.setup = function(){
+		strokeWeight(0.04);
+		textFont(f);
+		textSize(12);
+		ball = new FireBall(10, -20, 10, 2);
+		player = new Player();
+		maze = new Maze(22,12);
+		maze.setPlayerAtStart(player);
+		frameRate(60);
+		strokeWeight(2);
+	}
+	this.keyPressed = function(){
+		if (key == 'h') help = !help;
+		if(key=='+'){
+			player.pov.fovy -= 0.1;
+			player.updatePOV();
+		}
+		if(key=='-'){
+			player.pov.fovy += 0.1;
+		player.updatePOV();
+		}
+	}
+
+	this.draw = function(){
+		frameRate(60);
+		background(0, 0, 51);
+
+		if(frameCount % 30 === 0){
+			maze.checkLavaCollision(player);
+			ball.checkCollision(player);
+		}
 
 
-	maze.update();
-	maze.display();
-	player.update();
-	ball.display();
-	ball.update(player);
-	//drawAxes();
-  	if (help || frameCount < 400) { // Heads Up Display extension by jWilliam
+		maze.update();
+		maze.display();
+		player.update();
+		ball.display();
+		ball.update(player);
+		//drawAxes();
+		if (help || frameCount < 400) { // Heads Up Display extension by jWilliam
+			push(); // this affects the frame rate
+			camera(0, 0, (height / 2.0) / tan(PI * 30.0 / 180.0), 0, 0, 0, 0, 1, 0);
+			ortho(-width / 2, width / 2, -height / 2, height / 2, 0, 1000);
+			fill(0, 0, 0, 200);
+			noStroke();
+			translate(-380, -380, 0);
+			scale(2);
+			rect(0, 0, 140, 85);
+			fill(127);
+			text('mouse: left/right : pan', 10, 10);
+			text('       up/down : tilt', 10, 20);
+			text('       click : ptrlock', 10, 30);
+			text(' keys: a/d : left/right', 10, 40);
+			text('       w/s : fwd/bkwd', 10, 50);
+			text('       e/q : up/down', 10, 60);
+			text('       space : jump', 10, 70);
+			text('       h : help', 10, 80);
+			pop();
+	}
+
+		// if (startVisible) {
+		// 	startScreen();
+		// 	startVisible = false; // render only once
+		// }
+
+	}
+
+	if (help || frameCount < 400) { // Heads Up Display extension by jWilliam
 		push(); // this affects the frame rate
 		camera(0, 0, (height / 2.0) / tan(PI * 30.0 / 180.0), 0, 0, 0, 0, 1, 0);
 		ortho(-width / 2, width / 2, -height / 2, height / 2, 0, 1000);
@@ -103,72 +175,22 @@ function draw() {
 		text('       space : jump', 10, 70);
 		text('       h : help', 10, 80);
 		pop();
-}
-
-	if (startVisible) {
-		startScreen();
-		startVisible = false; // render only once
 	}
 
-}
-
-if (help || frameCount < 400) { // Heads Up Display extension by jWilliam
-	push(); // this affects the frame rate
-	camera(0, 0, (height / 2.0) / tan(PI * 30.0 / 180.0), 0, 0, 0, 0, 1, 0);
-	ortho(-width / 2, width / 2, -height / 2, height / 2, 0, 1000);
-	fill(0, 0, 0, 200);
-	noStroke();
-	translate(-380, -380, 0);
-	scale(2);
-	rect(0, 0, 140, 85);
-	fill(127);
-	text('mouse: left/right : pan', 10, 10);
-	text('       up/down : tilt', 10, 20);
-	text('       click : ptrlock', 10, 30);
-	text(' keys: a/d : left/right', 10, 40);
-	text('       w/s : fwd/bkwd', 10, 50);
-	text('       e/q : up/down', 10, 60);
-	text('       space : jump', 10, 70);
-	text('       h : help', 10, 80);
-	pop();
-}
-
-  
-if (startVisible) {
-	startScreen();
-	startVisible = false; // render only once
-}
-
-
-  
-
-	// function drawAxes(){
-	// 	push();
-	//       noStroke();
-	// 	  fill(127,0,0); // X red
-	// 	  translate(75,0.5,0.5);
-	// 	  box(150,1,1);
-	// 	pop();
-	// 	push();
-	//       noStroke();
-	// 	  fill(0,127,0); // Y green
-	// 	  translate(0.5,75,0.5);
-	// 	  box(1,150,1);
-	// 	pop();
-	// 	push();
-	//       noStroke();
-	// 	  fill(0,0,127); // Z blue
-	// 	  translate(0.5,0.5,75);
-	// 	  box(1,1,150);
-	// 	pop();
+	
+	// if (startVisible) {
+	// 	startScreen();
+	// 	startVisible = false; // render only once
 	// }
 
-	function mouseClicked() {
-	if (!player.pointerLock) {
-		player.pointerLock = true;
-		requestPointerLock();
-	} else {
-		exitPointerLock();
-		player.pointerLock = false;
+	this.mouseClicked = function(){
+		if (!player.pointerLock) {
+			player.pointerLock = true;
+			requestPointerLock();
+		} else {
+			exitPointerLock();
+			player.pointerLock = false;
+		}
 	}
+
 }
