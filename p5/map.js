6f94c9812cb0dbddbb44169b44070aea481dd5c4
cohenstevens <csteve54@kent.edu>
2024-03-18 14:53:18 -0400

    fixed issue with main

--- p5/map.js
+++ p5/map.js
@@ -9,7 +9,7 @@ class Block {
 		this.visited = false;
 	}
 
- 	update() {
+	update(reddish) {
 		let playerLeft = player.position.x - player.dimensions.x / 2;
 		let playerRight = player.position.x + player.dimensions.x / 2;
 		let playerTop = player.position.y - player.dimensions.y / 2;
@@ -57,6 +57,8 @@ class Block {
 					}
 				}
 		}
+		if(reddish == 'red') this.fillColor = 'red'; 
+		else this.fillColor = color(200)
     }
 
   	display() {
@@ -89,32 +91,47 @@ class FireBall {
 			// this.fillColor = color(random(150, 200));
 			this.texture = lava;
 			this.visited = false;
+			this.blockx = 0;
+			this.blockz = 0;
 		}
 
-	  	update(player) {
+		update(maze, player) {
 			let distance = dist(player.position.x, player.position.y, player.position.z, this.position.x, this.position.y, this.position.z);
 			let threshold = 75;
+			//console.log("Player position:", player.position.x, player.position.y, player.position.z);
+    		//console.log("Fireball position:", this.position.x, this.position.y, this.position.z);
 			if (distance < threshold) {
 				if(frameCount % 15 == 0)
 					this.checkCollision(player);
-				
+				/*
 				let para = createP("FIREBALL INCOMING!!");
 				para.class("fireball-notification");
 	
 				setTimeout(function() {
 					para.style("display", "none");
 				}, 2000);
+				*/
 			}
-
-    		this.position.y += 1; 
+			this.position.y += 1; 
 			if(this.position.y>10) {
 				this.position.y = -100;
 				//this.position.x = random(10,100);
 				//this.position.z = random(10,50);
-				this.position.x = player.position.x + random(10,75);
-				this.position.z = player.position.z + random(10,50);
+				// this.position.x = player.position.x + random(10,75);
+				// this.position.z = player.position.z + random(10,50);
+				this.blockx = Math.floor(random(1, maze.size1-1)); 
+				this.blockz = Math.floor(random(1, maze.size2-1));
+				if(this.blockx == maze.size1-1){
+					this.blockx = this.blockx-1; 
+				}
+				if(this.blockz == maze.size2-1){
+					this.blockz = this.blockz-1; 
+				}
+				this.position.x = this.blockx*5
+				this.position.z = this.blockz*5  
 			}
-  		}	
+		}
+
 
 		display() {
 			push();
@@ -140,19 +157,19 @@ class FireBall {
 }
 
 class Maze {
-  constructor(size1, size2) {
-    this.blocks = new Array(size1);
-
-    for (let i = 0; i < size1; i++) {
-      this.blocks[i] = new Array(size2);
-      for (let j = 0; j < size2; j++) {
-        let x = i * 5;
-        let y = 0;
-        let z = j * 5;
-        this.blocks[i][j] = new Block(x, y, z, 5, 5, 5, null);
-      }
-    }
-
+	constructor(size1, size2) {
+		this.blocks = new Array(size1);
+		this.size1 = size1; 
+		this.size2 = size2;
+		for (let i = 0; i < size1; i++) {
+		  this.blocks[i] = new Array(size2);
+		  for (let j = 0; j < size2; j++) {
+			let x = i * 5;
+			let y = 0;
+			let z = j * 5;
+			this.blocks[i][j] = new Block(x, y, z, 5, 5, 5, null);
+		  }
+		}
     this.start = this.blocks[1][2];
     //this.blocks[1][1].fillColor = color(63, 127, 63);
     // var m = [
@@ -178,10 +195,12 @@ class Maze {
   }
 
 
-  update() {
+  update(fireball) {
     for (let i = 0; i < this.blocks.length; i++) {
       for (let j = 0; j < this.blocks[i].length; j++) {
-        this.blocks[i][j].update();
+		if(fireball.blockx == i && fireball.blockz == j)
+        	this.blocks[i][j].update('red');
+		else this.blocks[i][j].update('none'); 
       }
     }
   }
