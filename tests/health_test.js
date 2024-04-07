e0ca9f8c8da8a5df89a2bd7c0fe3d64f22d32ea1
HadjAhmed <hadj_ahmed@aucegypt.edu>
2024-04-07 13:43:52 -0400

    successfully imported chai to health_test.js

--- tests/health_test.js
+++ tests/health_test.js
@@ -1,8 +1,9 @@
-import chai from 'chai';
+import * as chai from 'chai';
 var assert = chai.assert;
-
-assert.typeOf(foo, 'string');
-assert.equal(foo, 'bar');
-assert.lengthOf(foo, 3)
-assert.property(tea, 'flavors');
-assert.lengthOf(tea.flavors, 3);
+var foo = 'bar';
+var beverages = { tea: [ 'chai', 'matcha', 'oolong' ] };
+assert.typeOf(foo, 'string'); // without optional message
+assert.typeOf(foo, 'string', 'foo is a string'); // with optional message
+assert.equal(foo, 'bar', 'foo equal `bar`');
+assert.lengthOf(foo, 3, 'foo`s value has a length of 3');
+assert.lengthOf(beverages.tea, 3, 'beverages has 3 types of tea');
