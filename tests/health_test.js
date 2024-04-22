346ec60d44f72fc6ef3a9d40818f4fae7edb21b6
tbesser <tristan.besser58@gmail.com>
2024-04-21 20:38:07 -0400

    updated health_test.js to demonstrate the mocha testing with dummy data

--- tests/health_test.js
+++ tests/health_test.js
@@ -1,5 +1,6 @@
 import * as chai from 'chai';
-//import * as RoverCam from '../p5/p5.RoverCam.js'
+import * as RoverCam from '../p5/p5.RoverCam.js';
+// import * as hitSound from '../assets/hit.wav';
 var assert = chai.assert;
 // var foo = 'bar';
 // var beverages = { tea: [ 'chai', 'matcha', 'oolong' ] };
@@ -9,40 +10,46 @@ var assert = chai.assert;
 // assert.lengthOf(foo, 3, 'foo`s value has a length of 3');
 // assert.lengthOf(beverages.tea, 3, 'beverages has 3 types of tea');
 
+
+// Mock Player class with a takeHit() method
+class Player {
+    constructor() {
+        this.health = 100; // Initial health
+        this.dead = false;
+    }
+
+    takeHit() {
+        this.health -= 10;
+        if (this.health <= 0) {
+            this.dead = true;
+        }
+    }
+}
+
 describe('Player', () => {
-  let player;
+    let player;
 
-  beforeEach(() => {
-    player = new Player();
-  });
+    beforeEach(() => {
+        player = new Player();
+    });
 
-  it('should reduce health by 10 when hit', () => {
-    const initialHealth = player.health;
-    player.takeHit();
-    assert.strictEqual(player.health, initialHealth - 10);
-  });
+    it('should reduce health by 10 when hit', () => {
+        const initialHealth = player.health;
+        player.takeHit();
+        assert.strictEqual(player.health, initialHealth - 10);
+    });
 
-  it('should play hit sound when hit', () => {
-    player.takeHit();
-    // Assuming `hit` is an instance of the audio object
-    assert.strictEqual(hit.isPlaying(), true);
-  });
+    it('should set player as dead when health becomes 0', () => {
+        player.health = 10; // Set health to a non-zero value
+        player.takeHit(); // Reduce health to 0
+        assert.strictEqual(player.dead, true);
+    });
 
-  it('should set player as dead and play scream sound when health becomes 0', () => {
-    player.health = 10; // Set health to a non-zero value
-    player.takeHit(); // Reduce health to 0
-    assert.strictEqual(player.dead, true);
-    // Assuming `scream` is an instance of the audio object
-    assert.strictEqual(scream.isPlaying(), true);
-  });
-
-  it('should not reduce health if already dead', () => {
-    player.dead = true;
-    const initialHealth = player.health;
-    player.takeHit();
-    assert.strictEqual(player.health, initialHealth);
-    // Assuming hit sound doesn't play when dead
-    assert.strictEqual(hit.isPlaying(), false);
-  });
+    it('should not reduce health if already dead', () => {
+        player.dead = true;
+        const initialHealth = player.health;
+        player.takeHit();
+        assert.strictEqual(player.health, initialHealth);
+    });
 });
 
