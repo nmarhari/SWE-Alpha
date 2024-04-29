e3971fa9df13be3f3e6dbf6abac0e270e832d4ce
nmarhari <92116345+nmarhari@users.noreply.github.com>
2024-04-29 14:50:23 -0400

    ok!

--- test/headless.mjs
+++ test/headless.mjs
@@ -13,17 +13,14 @@ describe('test swe-alpha', async function(){
     beforeEach(async function(){
         driver = await new Builder()
             .forBrowser('chrome')
-            .setChromeOptions() // remove to see what is happening
+            .setChromeOptions(options.addArguments('--headless')) // remove to see what is happening
             .build();
         await driver.get('https://nmarhari.github.io/SWE-Alpha');
-        await sleep(5000);
+        await sleep(10000);
     })
 
     it('start the game & check health', async function(){
-        startButton = await driver.findElement(By.xpath('//button[@id="startButton"]'));
-        startButton.click();
-        startButton.click();
-        startButton.click();
+        await driver.executeScript('closeStartScreen()');
         await sleep(1000);
         healthBar = await driver.findElement(By.xpath('//div[@id="healthBarBorder"]'));
         await sleep(1000);
